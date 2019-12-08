17b3f89236a33068306a1e4a1472a4a60057380d
Olga Ivanova <olya@ivanovs.info>
2019-12-08 23:03:04 +0100

    render texture

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "lib/stb_image.h"
+
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO & SDL_INIT_JOYSTICK);
@@ -10,7 +14,7 @@ int main()
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
-        480,
+        980,
         SDL_WINDOW_OPENGL
     );
 
@@ -31,7 +35,30 @@ int main()
         return 1;
     }
 
-    SDL_Delay(3000);
+    int width, height, num_channels;
+    void *pixels = stbi_load("bd-sprites.png", &width, &height, &num_channels, 3);
+
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = width;
+    rect.h = height;
+
+    int result = SDL_UpdateTexture(texture, &rect, pixels, width*num_channels);
+    if (result != 0) {
+        printf("Couldn't update texture: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    result = SDL_RenderCopy(renderer, texture, &rect, &rect);
+    if (result != 0) {
+        printf("Couldn't renders texture: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_RenderPresent(renderer);
+    
+    SDL_Delay(7000);
 
     SDL_DestroyWindow(window);
 
